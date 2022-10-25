c752a6b5408db2a1e44bbb21341973717c0b0d30
igorbrasileiro <brasileiro456@gmail.com>
2022-10-25 18:01:33 -0300

    Make it work with coc.nvim

--- src/extension.ts
+++ src/extension.ts
@@ -1,213 +1,234 @@
-// Based on https://github.com/mjbvz/vscode-lit-html/blob/master/src/index.ts
-import * as vscode from 'vscode'
+// Based on https://github.com/tw-in-js/vscode-twind-intellisense/src/extension.ts
+import * as vscode from "coc.nvim";
 
-import {name, version as extensionVersion, publisher} from '../package.json'
+import { name, publisher, version as extensionVersion } from "../package.json";
 
-const typeScriptExtensionId = 'vscode.typescript-language-features'
-const pluginId = '@twind/typescript-plugin'
-const configurationSection = 'twind'
+const typeScriptExtensionId = "coc-tsserver";
+const pluginId = "@twind/typescript-plugin";
+const configurationSection = "twind";
 
-const extensionName = `${publisher}.${name}`
+const extensionName = `${publisher}.${name}`;
 
 interface SynchronizedConfiguration {
-  tags: ReadonlyArray<string>
-  attributes: ReadonlyArray<string>
-  styles: ReadonlyArray<string>
-  debug: boolean
-  enable: boolean
+  tags: ReadonlyArray<string>;
+  attributes: ReadonlyArray<string>;
+  styles: ReadonlyArray<string>;
+  debug: boolean;
+  enable: boolean;
   // Readonly validate: boolean;
   // readonly lint: { [key: string]: any };
   // readonly emmet: { [key: string]: any };
 }
 
-type Logger = (message: string) => void
+type Logger = (message: string) => void;
 
 interface State {
-  hasTwind?: boolean
+  hasTwind?: boolean;
 }
 
 export async function activate(context: vscode.ExtensionContext) {
-  const log = createLogger(vscode.window.createOutputChannel('Twind IntelliSense'))
-  log(`Extension Name: ${extensionName}.`)
-  log(`Extension Version: ${extensionVersion}.`)
+const log = createLogger(
+vscode.window.createOutputChannel("Twind IntelliSense"),
+);
+  
+  console.log(`Extension Name: ${extensionName}.`);
+  console.log(`Extension Version: ${extensionVersion}.`);
 
-  await enableExtension(context, log).catch((error) => {
-    log(`Activating ${pluginId} failed: ${error.stack}`)
-  })
+await enableExtension(context, log).catch((error) => {
+console.log(`Activating ${pluginId} failed: ${error.stack}`);
+});
 }
 
 async function enableExtension(context: vscode.ExtensionContext, log: Logger) {
-  const state: State = { hasTwind: undefined }
+  const state: State = { hasTwind: undefined };
 
   const update = async () => {
     const localTwindManifestFiles = await vscode.workspace.findFiles(
-      '**/node_modules/twind/package.json',
+      "**/node_modules/twind/package.json",
       null,
       1,
-    )
+    );
 
     const twindConfigFiles = localTwindManifestFiles.length
       ? []
-      : await vscode.workspace.findFiles('**/twind.config.{ts,js,mjs,cjs}', '**/node_modules/**', 1)
+      : await vscode.workspace.findFiles(
+        "**/twind.config.{ts,js,mjs,cjs}",
+        "**/node_modules/**",
+        1,
+      );
 
-    let hasTwind = false
+    let hasTwind = false;
     if (localTwindManifestFiles.length) {
-      log(`Using local twind`)
-      hasTwind = true
+      console.log(`Using local twind`);
+      hasTwind = true;
     } else if (twindConfigFiles.length) {
-      log(`Using builtin twind`)
-      hasTwind = true
+      console.log(`Using builtin twind`);
+      hasTwind = true;
     } else {
-      log(`No twind package and no twind config file found. Not activating Twind IntelliSense.`)
-      hasTwind = false
+      console.log(
+        `No twind package and no twind config file found. Not activating Twind IntelliSense.`,
+      );
+      hasTwind = false;
     }
 
     if (hasTwind !== state.hasTwind) {
-      state.hasTwind = hasTwind
-      synchronizeConfiguration(api, state, log)
+      state.hasTwind = hasTwind;
+      synchronizeConfiguration(api, state, log);
     }
-  }
+  };
 
-  const api = await activateTypescriptPlugin(log)
+  const api = await activateTypescriptPlugin(log);
+  console.log("Passei de API", api)
 
   const listener = () => {
     update().catch((error) => {
-      log(error.stacktrace)
-    })
-  }
+      console.log(error.stacktrace);
+    });
+  };
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('twind.restart', () => {
-      state.hasTwind = undefined
-      listener()
+    vscode.commands.registerCommand("twind.restart", () => {
+      state.hasTwind = undefined;
+      listener();
     }),
-  )
+  );
 
-  const replaceClassNameStrings = () => {
-    const editor = vscode.window.activeTextEditor
-    if (editor) {
-      const document = editor.document
-      const selection = editor.selection
-      const word = document.getText(selection)
-      const replaced = word.replaceAll(/className="([^"].*)"/g, `className={tw\`$1\`}`)
-      editor.edit((editBuilder) => {
-        editBuilder.replace(selection, replaced)
-      })
-    }
-  }
+  // const replaceClassNameStrings = () => {
+  // const editor = vscode.window.activeTextEditor
+  // if (editor) {
+  // const document = editor.document
+  // const selection = editor.selection
+  // const word = document.getText(selection)
+  // const replaced = word.replaceAll(/className="([^"].*)"/g, `className={tw\`$1\`}`)
+  // editor.edit((editBuilder) => {
+  // editBuilder.replace(selection, replaced)
+  // })
+  // }
+  // }
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand('twind.className', () => {
-      replaceClassNameStrings()
-    }),
-  )
+  // context.subscriptions.push(
+  // vscode.commands.registerCommand('twind.className', () => {
+  // replaceClassNameStrings()
+  // }),
+  // )
 
   const twindWatcher = vscode.workspace.createFileSystemWatcher(
-    '**/node_modules/twind/package.json',
-  )
+    "**/node_modules/twind/package.json",
+  );
 
-  context.subscriptions.push(twindWatcher)
-  context.subscriptions.push(twindWatcher.onDidCreate(listener))
-  context.subscriptions.push(twindWatcher.onDidChange(listener))
-  context.subscriptions.push(twindWatcher.onDidDelete(listener))
+  context.subscriptions.push(twindWatcher);
+  context.subscriptions.push(twindWatcher.onDidCreate(listener));
+  context.subscriptions.push(twindWatcher.onDidChange(listener));
+  context.subscriptions.push(twindWatcher.onDidDelete(listener));
 
-  const configWatcher = vscode.workspace.createFileSystemWatcher('**/twind.config.{ts,js,mjs,cjs}')
+  const configWatcher = vscode.workspace.createFileSystemWatcher(
+    "**/twind.config.{ts,js,mjs,cjs}",
+  );
 
-  context.subscriptions.push(configWatcher)
-  context.subscriptions.push(configWatcher.onDidCreate(listener))
-  context.subscriptions.push(configWatcher.onDidDelete(listener))
+  context.subscriptions.push(configWatcher);
+  context.subscriptions.push(configWatcher.onDidCreate(listener));
+  context.subscriptions.push(configWatcher.onDidDelete(listener));
 
   const packageWatcher = vscode.workspace.createFileSystemWatcher(
-    '**/{package,package-lock.json,yarn.lock,pnpm-lock.yaml}',
-  )
+    "**/{package,package-lock.json,yarn.lock,pnpm-lock.yaml}",
+  );
 
-  context.subscriptions.push(packageWatcher)
-  context.subscriptions.push(packageWatcher.onDidCreate(listener))
-  context.subscriptions.push(packageWatcher.onDidCreate(listener))
-  context.subscriptions.push(packageWatcher.onDidDelete(listener))
+  context.subscriptions.push(packageWatcher);
+  context.subscriptions.push(packageWatcher.onDidCreate(listener));
+  context.subscriptions.push(packageWatcher.onDidCreate(listener));
+  context.subscriptions.push(packageWatcher.onDidDelete(listener));
 
-  await update()
+  await update();
 }
 
 async function activateTypescriptPlugin(log: Logger) {
-  const extension = vscode.extensions.getExtension(typeScriptExtensionId)
-  if (!extension) {
-    log(`Extension ${typeScriptExtensionId} not found. No IntelliSense will be provided.`)
-    return
+  vscode.extensions.isActivated(typeScriptExtensionId);
+  const extensions = vscode.extensions.all
+  extensions
+  const extension = vscode.extensions.all.find((ext) => ext.id === typeScriptExtensionId);
+  if (
+    !extension
+  ) {
+    console.log(
+      `Extension ${typeScriptExtensionId} not found. No IntelliSense will be provided.`,
+    );
+    return;
   }
 
-  await extension.activate()
+  await extension.activate();
 
-  const api = extension.exports?.getAPI?.(0)
+  const api = extension.exports;
 
   if (!api) {
-    log(
+    console.log(
       `Extension ${typeScriptExtensionId} did not export an API. No IntelliSense will be provided.`,
-    )
-    return
+    );
+    return;
   }
 
-  return api
+  return api;
 }
 
 function createLogger(outputChannel: vscode.OutputChannel): Logger {
   return (message) => {
-    const title = new Date().toLocaleTimeString()
-    outputChannel.appendLine(`[${title}] ${message}`)
-  }
+    const title = new Date().toLocaleTimeString();
+    outputChannel.appendLine(`[${title}] ${message}`);
+  };
 }
 
 function synchronizeConfiguration(api: any, state: State, log: Logger) {
-  const config = getConfiguration()
+  const config = getConfiguration();
 
   if (config.enable) {
-    log('Extension is enabled. To disable, change the `twind.enable` setting to `false`.')
+    console.log(
+      "Extension is enabled. To disable, change the `twind.enable` setting to `false`.",
+    );
 
     if (state.hasTwind) {
-      log(`Configuring ${pluginId} using: ${JSON.stringify(config, null, 2)}`)
+      console.log(`Configuring ${pluginId} using: ${JSON.stringify(config, null, 2)}`);
     } else {
-      config.enable = false
+      config.enable = false;
     }
   } else {
-    log(
-      'Extension is disabled. No IntelliSense will be provided. To enable, change the `twind.enable` setting to `true`.',
-    )
+    console.log(
+      "Extension is disabled. No IntelliSense will be provided. To enable, change the `twind.enable` setting to `true`.",
+    );
   }
 
-  api.configurePlugin(pluginId, config)
+  api.configurePlugin(pluginId, config);
 }
 
 function getConfiguration(): SynchronizedConfiguration {
-  const config = vscode.workspace.getConfiguration(configurationSection)
+  const config = vscode.workspace.getConfiguration(configurationSection);
   const outConfig: SynchronizedConfiguration = {
-    tags: ['tw', 'apply'],
-    attributes: ['tw', 'class', 'className'],
-    styles: ['style', 'styled'],
+    tags: ["tw", "apply"],
+    attributes: ["tw", "class", "className"],
+    styles: ["style", "styled"],
     debug: false,
     enable: true,
-  }
+  };
 
-  withConfigValue<string[]>(config, 'tags', (tags) => {
-    outConfig.tags = tags
-  })
+  withConfigValue<string[]>(config, "tags", (tags) => {
+    outConfig.tags = tags;
+  });
 
-  withConfigValue<string[]>(config, 'attributes', (attributes) => {
-    outConfig.attributes = attributes
-  })
+  withConfigValue<string[]>(config, "attributes", (attributes) => {
+    outConfig.attributes = attributes;
+  });
 
-  withConfigValue<string[]>(config, 'styles', (styles) => {
-    outConfig.styles = styles
-  })
+  withConfigValue<string[]>(config, "styles", (styles) => {
+    outConfig.styles = styles;
+  });
 
-  withConfigValue<boolean>(config, 'debug', (debug) => {
-    outConfig.debug = debug
-  })
+  withConfigValue<boolean>(config, "debug", (debug) => {
+    outConfig.debug = debug;
+  });
 
-  withConfigValue<boolean>(config, 'enable', (enable) => {
-    outConfig.enable = enable
-  })
+  withConfigValue<boolean>(config, "enable", (enable) => {
+    outConfig.enable = enable;
+  });
 
-  return outConfig
+  return outConfig;
 }
 
 function withConfigValue<T>(
@@ -215,23 +236,22 @@ function withConfigValue<T>(
   key: string,
   withValue: (value: T) => void,
 ): void {
-  const configSetting = config.inspect(key)
+  const configSetting = config.inspect(key);
   if (!configSetting) {
-    return
+    return;
   }
 
   // Make sure the user has actually set the value.
   // VS Code will return the default values instead of `undefined`, even if user has not don't set anything.
   if (
-    typeof configSetting.globalValue === 'undefined' &&
-    typeof configSetting.workspaceFolderValue === 'undefined' &&
-    typeof configSetting.workspaceValue === 'undefined'
+    typeof configSetting.globalValue === "undefined" &&
+    typeof configSetting.workspaceValue === "undefined"
   ) {
-    return
+    return;
   }
 
-  const value = config.get<T | undefined>(key, undefined)
-  if (typeof value !== 'undefined') {
-    withValue(value)
+  const value = config.get<T | undefined>(key, undefined);
+  if (typeof value !== "undefined") {
+    withValue(value);
   }
 }
